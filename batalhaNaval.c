7259083357736acd09c43cd1c088d6c4e3d94a73
André Victor <andre899120@gmail.com>
2026-04-13 23:22:00 +0000

    Desafio aventureiro concluído

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,49 +6,43 @@
 int main() {
     int navio1[3] = {3, 3, 3};
     int navio2[3] = {3, 3, 3};
+    int navio3[3] = {3, 3, 3};
+    int navio4[3] = {3, 3, 3};
     char coordenadaX[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int tabuleiro[10][10] = // Tabuleiro com 10 linhas e 10 colunas, 0 representa a água e posteriormente 3 representará os navios
-    {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    int tabuleiro[10][10]; // Tabuleiro com 10 linhas e 10 colunas, 0 representa a água e posteriormente 3 representará os navios
+    
     printf("*** Tabuleiro Batalha Naval ***\n");
-    // Navio 1 posicionado horizontalmente na linha 3, colunas G, H e I
-    tabuleiro[3][6] = navio1[0];
-    tabuleiro[3][7] = navio1[1];
-    tabuleiro[3][8] = navio1[2];
-    // Navio 2 posicionado verticalmente na coluna 2, linhas 6, 7 e 8
-    tabuleiro[6][2] = navio2[0];
-    tabuleiro[7][2] = navio2[1];
-    tabuleiro[8][2] = navio2[2];
     printf("   ");
     for (int i = 0; i < 10; i++)
     {
-        printf("%c  ", coordenadaX[i]);
+        printf("%c  ", coordenadaX[i]); // Coordenadas com letras de A a J para as colunas
     }
     printf("\n");
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            tabuleiro[i][j] = 0; // Demarcados com 0 indicando que é água inicialmente
+        }
+    }
+    
+    for (int i = 0; i < 3; i++)
+    {
+        tabuleiro[3][i + 2] = navio1[i]; // Posiciona o navio1 horizontalmente na posição (3, C) (3, D) (3, E)
+        tabuleiro[i + 5][2] = navio2[i]; // Posiciona o navio2 verticalmente na posição (5, C) (6, C) (7, C)
+        tabuleiro[i + 2][i + 6] = navio3[i]; // Posiciona o navio3 diagonalmente na posição (2, G) (3, H) (4, I)
+        tabuleiro[i + 5][i + 6] = navio4[i]; // Posiciona o navio4 diagonalmente na posição (5, G) (5, H) (6, I)
+    }
+
     for (int i = 0; i < 10; i++)
     {
         printf("%d  ", i);
         for (int j = 0; j < 10; j++)
         {
-            printf("%d  ", tabuleiro[i][j]);
+            printf("%d  ", tabuleiro[i][j]); // Exibindo o tabuleiro completo com 0 para água e 3 para navios
         }
         printf("\n");
     }
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
